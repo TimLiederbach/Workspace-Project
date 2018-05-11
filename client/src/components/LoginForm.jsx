b634f736843e64c23bddba25c65158af85f05822
Student_105 <student_105@ool-ad020213.dyn.optonline.net>
2018-05-11 14:07:13 -0400

    form and nav

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,106 @@
+import './LoginForm.css';
+
+// import React from 'react'
+// import { Button, Form, Message } from 'semantic-ui-react'
+
+// const FormExampleSuccess = () => (
+//   <Form success>
+//     <Form.Input label='email'
+//         placeholder='joe@schmoe.com'
+//         type='text'
+//         onChange={this.handleInputChange}
+//         value={this.state.email}
+//         name='email'/>
+//     <Message
+//       success
+//       header='Form Completed'
+//       content="You're all signed up for the newsletter"
+//     />
+//     <Button>Submit</Button>
+//   </Form>
+// )
+
+// export default FormExampleSuccess
+
+import React, { Component } from 'react';
+import { Button, Form, Message } from 'semantic-ui-react'
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onLogin(this.state);
+    this.setState({
+      userName: '',
+      email: '',
+      password: ''
+    });
+  }
+
+  render() {
+    return (
+    <Form success className='form'>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          User Name:
+          <input
+            type='text'
+            onChange={this.handleInputChange}
+            value={this.state.userName}
+            name='userName'
+          />
+        </label>
+
+
+
+        <label>
+          Email:
+          <input
+            type='text'
+            onChange={this.handleInputChange}
+            value={this.state.email}
+            name='email'
+          />
+        </label>
+
+
+        <label>
+          Password:
+          <input
+            type='password'
+            onChange={this.handleInputChange}
+            value={this.state.password}
+            name='password'
+          />
+        </label>
+          <br />
+        <Button type='submit' className='login-button'>Login</Button>
+        <Message
+            success
+            header='Form Completed'
+            content="You're all signed up for WrkSps"
+          />
+
+      </form>
+    </Form>
+    )
+  }
+}
+
+export default LoginForm;
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Menu, Icon } from 'semantic-ui-react'
+
+export default class MenuExampleStackable extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu stackable>
+        <Menu.Item>
+
+          <Icon name='computer' size='huge' />
+
+        </Menu.Item>
+
+        <Menu.Item
+          name='features'
+          active={activeItem === 'features'}
+          onClick={this.handleItemClick}
+        >
+          Features
+        </Menu.Item>
+
+        <Menu.Item
+          name='testimonials'
+          active={activeItem === 'testimonials'}
+          onClick={this.handleItemClick}
+        >
+          Testimonials
+        </Menu.Item>
+
+        <Menu.Item
+          name='sign-in'
+          active={activeItem === 'sign-in'}
+          onClick={this.handleItemClick}
+        >
+          Sign-in
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
