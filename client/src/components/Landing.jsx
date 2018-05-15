47fc75c3fd5919518d7823ffb3299f99598b0da0
Social Impact (6) <lz28330@gmail.com>
2018-05-15 17:31:48 -0400

    slider

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Slider from './Slider';
 
 function Landing (props)  {
 
 return (
   <div>
     <h1>This is the cool HD carousel of photos...think about it</h1>
+    <Slider />
     <ul>
       <li><Link to='/workspaces'>Workspaces</Link></li>
     </ul>
