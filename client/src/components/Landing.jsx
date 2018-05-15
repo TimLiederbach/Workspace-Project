6a832c399e1ec1d0d319b8a1f268fb5d1bf7ed76
tim liederbach <timmy@ool-ad020772.dyn.optonline.net>
2018-05-15 19:05:11 -0400

    carousel feature

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MySlider from './MySlider';
 
 function Landing (props)  {
 
 return (
   <div>
     <h1>This is the cool HD carousel of photos...think about it</h1>
+    <MySlider />
     <ul>
       <li><Link to='/workspaces'>Workspaces</Link></li>
     </ul>
@@ -14,3 +16,4 @@ return (
 }
 
 export default Landing;
+
